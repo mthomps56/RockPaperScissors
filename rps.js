192aea0d1536c768b95ae9a25c98a2bfcabb3d47
mthomps <mattthompson56@gmail.com>
2024-12-24 21:40:07 -0500

    Player and computer input returning properly. Button hightlighting working properly

--- rps.js
+++ rps.js
@@ -3,42 +3,46 @@ function getComputerChoice() {
   
   // Get 0, 1, or 2 and convert to Computer's choice.
   let convertChoice = Math.floor(Math.random() * 3);
+  console.log(convertChoice); 
   switch (convertChoice) {
-    case: 0:
-      const rock = document.querySelector('#r');
-      rock.style.background = "purple";
+    case 0:
+      const r = document.querySelector('#r');
+      r.style.background = "purple";
       compChoice = 'rock';
       break;
     case 1:
-      const paper = document.querySelector('#p');
-      paper.style.background = "purple";
+      const p = document.querySelector('#p');
+      p.style.background = "purple";
       compChoice = 'paper';
       break;
     case 2:
-      const scissors = document.querySelector('#p');
-      paper.style.background = "purple";
+      const s = document.querySelector('#s');
+      s.style.background = "purple";
       compChoice = 'scissors';
       break;
   }
-  
+  console.log(compChoice);
   return compChoice;
 }
 
 function getHumanChoice(event) {
+  console.log(event.target.id);
   let humanChoice;
-  switch (event.target.id) {
-    case 'r':
-      humanChoice = 'rock';
-      break;
-    case 'p':
-      humanChoice = 'paper';
-      break;
-    case 's':
-      humanChoice = 'scissors';
-      break;
-    }
-  event.target.style.background = "green";
-  return humanChoice;
+  if (event.target.id != 'bC') {
+    switch (event.target.id) {
+      case 'r':
+        humanChoice = 'rock';
+        break;
+      case 'p':
+        humanChoice = 'paper';
+        break;
+      case 's':
+        humanChoice = 'scissors';
+        break;
+      }
+    event.target.style.background = "green";
+    return humanChoice;
+  }
 }
 
 //Capitalize the choices for beauty's sake.
@@ -90,38 +94,25 @@ function playRound(humanChoice, computerChoice) {
 }
 
 // Start Game
+let pScore = 0, cScore = 0;
 
 let winner, humanChoice, computerChoice, counter = 0;
-let humanScore = 0, computerScore = 0;
 
-while (counter < 5) {
-  humanChoice = getHumanChoice();
-  computerChoice = getComputerChoice();
-  winner =  playRound(humanChoice, computerChoice);
+let buttonContainer = document.querySelector('.buttonContainer');
 
-  switch (winner) {
-    case 'computer':
-      computerScore += 1;
-      break;
-    case 'player':
-      humanScore += 1;
-    case 'tie':
-      break;
-  }
+buttonContainer.addEventListener('click', (e) => {
+  humanChoice = getHumanChoice(e);
+  computerChoice = getComputerChoice(e);
+});
 
-  winner = toTitleCase(winner);
-  humanChoice = toTitleCase(humanChoice);
-  computerChoice = toTitleCase(computerChoice);
 
-  alert(`Winner is ${winner}.\nPlayer chooses ${humanChoice}
+/*
+alert(`Winner is ${winner}.\nPlayer chooses ${humanChoice}
          Computer chooses ${computerChoice}\nScore: Player-
          ${humanScore}\t Computer-${computerScore}`);
+         */
 
-  counter += 1;
-}
-/////////////////////////
 
-let buttonContainer = document.querySelector('.buttonContainer', (event) => {
 
   
 
